fbf88d0e178e78f29af05cd1e7ea51e9605ba4e6
Beqs <benny.hong@hotmail.se>
2017-09-12 10:03:57 +0200

    Test2

--- Sk1/Program.cs
+++ Sk1/Program.cs
@@ -6,7 +6,8 @@ namespace Sk1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
+            Console.ReadLine();
         }
     }
 }
